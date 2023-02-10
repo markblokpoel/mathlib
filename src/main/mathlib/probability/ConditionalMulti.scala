765d9e3f4d53d18a5fb8f8b5043b0dcdd46cfc33
markblokpoel <mark.blokpoel@gmail.com>
2023-02-10 11:21:33 +0100

    New probability implementation
    Removed unnecessary files.

--- src/main/mathlib/probability/ConditionalMulti.scala
+++
@@ -1,7 +0,0 @@
-package mathlib.probability
-
-import mathlib.probability.datastructures.DiscreteDomain
-
-case class ConditionalMulti (value: DiscreteDomain, prior: ConditionalMulti) {
-  override def toString: String = s"$value | $prior"
-}
