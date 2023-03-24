1124f1934c5cd6e528666e1d5f8e84cc0f2393ca
Mark Blokpoel <mark.blokpoel@gmail.com>
2023-03-24 16:19:58 +0100

    Weighted Directed Graph

--- src/main/mathlib/demos/Coherence.scala
+++ src/main/mathlib/demos/Coherence.scala
@@ -1,23 +1,23 @@
 package mathlib.demos
 
 import mathlib.graph.GraphImplicits._
-import mathlib.graph.{DiGraph, WUnDiGraph}
+import mathlib.graph.{DiGraph, WDiGraph, WUnDiGraph}
 
 object Coherence {
 
   def main(args: Array[String]): Unit = {
 
-    val network = DiGraph.empty +
-      N("A") ~> N("B")  +
-      N("A") ~> N("C")  +
-      N("B") ~> N("C") +
-      N("C") ~> N("D")  +
-      N("C") ~> N("E") +
-      N("C") ~> N("F")
+    val network = WDiGraph.empty +
+      N("A") ~> N("B") % 1.0 +
+      N("A") ~> N("C") % 1.0 +
+      N("B") ~> N("C") % 1.0 +
+      N("C") ~> N("D") % 1.0 +
+      N("C") ~> N("E") % 1.0 +
+      N("C") ~> N("F") % 1.0
 
     println(network)
 
-    val e1 = N("A") ~> N("B")
+    val e1 = N("A") ~> N("B") % 1.0
 
     println(network.nextLeft(e1))
     println(network.nextRight(e1))
---
+++ src/main/mathlib/graph/WDiGraph.scala
@@ -0,0 +1,44 @@
+package mathlib.graph
+import scala.reflect.ClassTag
+
+case class WDiGraph[T](override val vertices: Set[Node[T]],override val edges: Set[WDiEdge[Node[T]]])
+  extends Graph[T, WDiEdge[Node[T]]](vertices, edges) {
+  override def +(vertex: Node[T]): WDiGraph[T] =
+    WDiGraph(vertices + vertex, edges)
+
+  override def +(_vertices: Set[Node[T]]): WDiGraph[T] =
+    _vertices.foldLeft(this)(_ + _)
+
+  override def +(edge: WDiEdge[Node[T]]): WDiGraph[T] =
+    WDiGraph(vertices + edge.v1 + edge.v2, edges + edge)
+
+  override def +[X: ClassTag](_edges: Set[WDiEdge[Node[T]]]): WDiGraph[T] =
+    _edges.foldLeft(this)(_ + _)
+
+  override def -(vertex: Node[T]): WDiGraph[T] =
+    WDiGraph(vertices - vertex, edges.filter(_ match {
+      case edge: WDiEdge[Node[T]] => !edge.contains(vertex)
+    }))
+
+  override def -(_vertices: Set[Node[T]]): WDiGraph[T] =
+    _vertices.foldLeft(this)(_ - _)
+
+  override def -(edge: WDiEdge[Node[T]]): WDiGraph[T] =
+    WDiGraph(vertices, edges - edge)
+
+  override def -[X: ClassTag](_edges: Set[WDiEdge[Node[T]]]): WDiGraph[T] =
+    _edges.foldLeft(this)(_ - _)
+
+  override def merge[G <: Graph[T, WDiEdge[Node[T]]]](that: G): WDiGraph[T] = that match {
+    case WDiGraph(thatVertices: Set[Node[T]], thatEdges: Set[WDiEdge[Node[T]]]) =>
+      val v: Set[Node[T]] = this.vertices union thatVertices
+      val e = this.edges union thatEdges
+      WDiGraph(v, e)
+  }
+}
+
+case object WDiGraph {
+  def apply[T](vertices: Set[Node[T]]): WDiGraph[T] = WDiGraph.empty + vertices
+  def apply[T, X: ClassTag](edges: Set[WDiEdge[Node[T]]]): WDiGraph[T] = WDiGraph.empty + edges
+  def empty[T]: WDiGraph[T] = WDiGraph(Set.empty, Set.empty)
+}
