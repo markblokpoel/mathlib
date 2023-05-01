2de89c3c48084ea1339ddefb052d6cf0ba8b4044
markblokpoel <mark.blokpoel@gmail.com>
2023-05-01 14:05:52 +0200

    Equals implementations for edges

--- src/main/mathlib/graph/WDiEdge.scala
+++ src/main/mathlib/graph/WDiEdge.scala
@@ -2,4 +2,22 @@ package mathlib.graph
 
 import mathlib.graph.properties.{Edge, ProtoEdge, WeightedEdge}
 
-case class WDiEdge[T <: Node[_]](override val left: T, override val right: T, weight: Double) extends Edge[T](left, right) with WeightedEdge with ProtoEdge[T]
+case class WDiEdge[T <: Node[_]](override val left: T, override val right: T, weight: Double)
+    extends Edge[T](left, right)
+    with WeightedEdge
+    with ProtoEdge[T] {
+  override def canEqual(that: Any): Boolean = that.isInstanceOf[WDiEdge[T]]
+
+  override def equals(obj: Any): Boolean =
+    obj match {
+      case that: WDiEdge[T] => {
+        left == that.left && right == that.right
+      }
+      case _ => false
+    }
+
+  override def hashCode(): Int = {
+    val prime = 1693
+    (prime + left.hashCode()) * prime + right.hashCode()
+  }
+}
--- src/main/mathlib/graph/WUnDiEdge.scala
+++ src/main/mathlib/graph/WUnDiEdge.scala
@@ -3,5 +3,20 @@ package mathlib.graph
 import mathlib.graph.properties.{Edge, ProtoEdge, WeightedEdge}
 
 case class WUnDiEdge[T <: Node[_]](override val left:T, override val right: T, weight: Double) extends Edge[T](left, right) with WeightedEdge with ProtoEdge[T] {
+  override def canEqual(that: Any): Boolean = that.isInstanceOf[UnDiEdge[T]]
 
+  override def equals(obj: Any): Boolean =
+    obj match {
+      case that: UnDiEdge[T] => {
+        left == that.left && right == that.right ||
+          left == that.right && right == that.left
+      }
+      case _ => false
+    }
+
+  override def hashCode(): Int = {
+    val prime = 1693
+    (prime + left.hashCode()) * prime + right.hashCode() +
+      (prime + right.hashCode()) * prime + left.hashCode()
+  }
 }
