9c1fc480a9be2f6292de3aab88863764bcd7491e
markblokpoel <mark.blokpoel@gmail.com>
2023-05-01 15:51:06 +0200

    HyperGraph implementation
    Cleanup

--- src/main/mathlib/graph/GraphImplicits.scala
+++ src/main/mathlib/graph/GraphImplicits.scala
@@ -24,7 +24,6 @@ object GraphImplicits {
     def %(weight: Double): WUnDiEdge[T] = WUnDiEdge(unDiEdge.left, unDiEdge.right, weight)
   }
 
-
   implicit class DiHyperEdgeImpl1[T](protoNodeSet1: Set[T]) {
     def ~>(protoNodeSet2: Set[T]): DiHyperEdge[Node[T]] =
       DiHyperEdge(protoNodeSet1.map(N), protoNodeSet2.map(N))
@@ -52,35 +51,4 @@ object GraphImplicits {
   implicit class WUnDiHyperEdgeImpl[T](diHyperEdge: UnDiHyperEdge[Node[T]]) {
     def %(weight: Double): WUnDiHyperEdge[Node[T]] = WUnDiHyperEdge(diHyperEdge.left, diHyperEdge.right, weight)
   }
-
-
-  //  implicit class UnDiHyperEdgeImpl1a[T <: Node[_]](unDiEdge: UnDiEdge[T]) {
-////    def ~(node: T): UnDiHyperEdge[T] = UnDiHyperEdge(Set(unDiEdge.left, unDiEdge.right, node))
-//  }
-//
-//  implicit class UnDiHyperEdgeImpl1b[T](unDiEdge: UnDiEdge[Node[T]]) {
-//    def ~(protoNode1: T): UnDiHyperEdge[Node[T]] = UnDiHyperEdge(
-//      Set(unDiEdge.left, unDiEdge.right, Node(protoNode1))
-//    )
-//  }
-//
-//  implicit class UnDiHyperEdgeImpl2a[T <: Node[_]](unDiHyperEdge: UnDiHyperEdge[T]) {
-////    def ~(node: T): UnDiHyperEdge[T] = UnDiHyperEdge(unDiHyperEdge.nodes + node)
-//    def %(weight: Double): WUnDiHyperEdge[T] =
-//      new WUnDiHyperEdge(unDiHyperEdge.left, unDiHyperEdge.right, weight)
-//  }
-
-//  implicit class UnDiHyperEdgeImpl2b[T](unDiHyperEdge: UnDiHyperEdge[Node[T]]) {
-////    def ~(protoNode: T): UnDiHyperEdge[Node[T]] = UnDiHyperEdge(unDiHyperEdge.left, unDiHyperEdge.right + Node(protoNode))
-//  }
-
-
-
-
-  implicit class DiHyperEdgeImpl2a[T <: Node[_]](diHyperEdge: DiHyperEdge[T]) {
-    def %(weight: Double): WDiHyperEdge[T] =
-      new WDiHyperEdge(diHyperEdge.left, diHyperEdge.right, weight)
-  }
-
-
 }
