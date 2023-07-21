96cc658f87bb6863ba87f27a5b9217f27273f5e2
markblokpoel <mark.blokpoel@gmail.com>
2023-07-21 14:11:23 +0200

    Documentation
    Added scaladoc to  HyperEdge and WeightedEdge.

--- src/main/scala/mathlib/graph/properties/HyperEdge.scala
+++ src/main/scala/mathlib/graph/properties/HyperEdge.scala
@@ -2,9 +2,35 @@ package mathlib.graph.properties
 
 import mathlib.graph.Node
 
+/** Abstract class to represent a basic hyper edge.
+  * @param left
+  *   The left side of the hyper edge, namely a list of vertices.
+  * @param right
+  *   The right side of the hyper edge, namely a list of vertices.
+  * @tparam T
+  *   The type of the vertices that belong to this edge.
+  */
 abstract class HyperEdge[T <: Node[_]](left: Iterable[T], right: Iterable[T]) extends ProtoEdge[T] {
+
+  /** Checks if vertex is contained in the hyper edge.
+    *
+    * @param vertex
+    *   The set of vertices to test.
+    * @return
+    * ```true``` if vertex exists in either the left side or the right side of the hyper edge,
+    * ```false``` otherwise.
+    */
   def contains(vertex: T): Boolean =
     left.exists(_ == vertex) || right.exists(_ == vertex)
-  def contains(vertex: Set[T]): Boolean =
-    vertex.forall(v => left.exists(_ == v)) || vertex.forall(v => right.exists(_ == v))
+
+  /** Checks if vertices is contained in the hyper edge.
+    *
+    * @param vertices
+    *   The set of vertices to test.
+    * @return
+    * ```true``` if vertices is a subset of either the left side or the right side of the hyper
+    * edge, ```false``` otherwise.
+    */
+  def contains(vertices: Set[T]): Boolean =
+    vertices.forall(v => left.exists(_ == v)) || vertices.forall(v => right.exists(_ == v))
 }
--- src/main/scala/mathlib/graph/properties/WeightedEdge.scala
+++ src/main/scala/mathlib/graph/properties/WeightedEdge.scala
@@ -1,5 +1,7 @@
 package mathlib.graph.properties
 
+/** Trait for weighted edges. */
 trait WeightedEdge {
+  /** Weight of the edge. */
   val weight: Double
 }
