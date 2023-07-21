fe1ea71b7fed94267ab1a38d4ed69e7c53d58355
markblokpoel <mark.blokpoel@gmail.com>
2023-07-21 14:00:03 +0200

    Documentation
    Added scaladoc to ProtoEdge.

--- src/main/scala/mathlib/graph/properties/ProtoEdge.scala
+++ src/main/scala/mathlib/graph/properties/ProtoEdge.scala
@@ -2,4 +2,8 @@ package mathlib.graph.properties
 
 import mathlib.graph.Node
 
+/** Proto trait for all non-hyper edges.
+  * @tparam T
+  *   The type of the vertices that belong to this edge.
+  */
 trait ProtoEdge[T <: Node[_]]
