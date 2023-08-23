55ece9aa8577d3778d2a3aff5d14a25cb872782c
Mark Blokpoel <mark.blokpoel@gmail.com>
2023-08-23 14:53:30 +0200

    JOSS
    Initialization

--- src/main/scala/mathlib/probability/Conditional1.scala
+++
@@ -1,9 +0,0 @@
-package mathlib.probability
-
-case class Conditional1[A, B](value: A, condition: B) {
-  override def toString: String = s"$value | $condition"
-}
-
-case class Conditional2[A, B](value: A, prior: Distribution[B]) {
-  override def toString: String = s"$value | $prior"
-}
