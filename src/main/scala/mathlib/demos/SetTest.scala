fef577b8f2ccda6584600e6a315cb1699623b692
Mark Blokpoel <mark.blokpoel@gmail.com>
2025-02-13 13:19:41 +0100

    Added summation for set with included builder.

--- src/main/scala/mathlib/demos/SetTest.scala
+++ src/main/scala/mathlib/demos/SetTest.scala
@@ -6,12 +6,14 @@ object SetTest {
   def main(args: Array[String]): Unit = {
     def len(s: String): Int = s.length
     def len2(s: (String, String)): Int = (s._1 + s._2).length
+    def isEven(s: String): Boolean = (len(s) % 2) == 0
 
     Seq(
       (sum(Set("a","b","aa","bb","ccc", "ddd"), len _), 12),
       (product(Set("a", "b", "aa", "bb", "ccc", "ddd"), len _), 1*1*2*2*3*3),
       (sum(Set(("a", "b"), ("aa", "bb"), ("ccc", "ddd")), len2 _), 2+4+6),
-      (product(Set(("a", "b"), ("aa", "bb"), ("ccc", "ddd")), len2 _), 2*4*6)
+      (product(Set(("a", "b"), ("aa", "bb"), ("ccc", "ddd")), len2 _), 2*4*6),
+      (sum(Set("a","b","aa","bb","ccc", "ddd"), isEven _, len _), 4)
     ).foreach(qa => println(f"${qa._1 == qa._2} <- ${qa._1} == ${qa._2}"))
   }
 }
